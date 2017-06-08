b18103bd88d5ff19c495d3116b6b7bfdbd59f262
nadav93 <nadav93@gmail.com>
2017-06-07 20:17:57 -0400

    jetpack practice

--- class11/d3-jetpack.js
+++ class11/d3-jetpack.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.d3 = global.d3 || {})));
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (factory((global.d3 = global.d3 || {})));
 }(this, (function (exports) { 'use strict';
 
 var version = "4.7.3";
